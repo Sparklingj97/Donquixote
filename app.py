088bf74a96808651b534796883c350a7be0a3a46
An-Woojin <woojin9710@naver.com>
2016-01-11 14:23:11 +0900

    PyCharm synced!!

--- app.py
+++ app.py
@@ -2,12 +2,16 @@ from flask import (Flask, render_template)
 app = Flask(__name__)
 
 @app.route("/")
-def main():
+def login():
     return render_template(
         "login.html"
     )
 
-@app.route()
+@app.route("/index")
+def main():
+    return render_template(
+        "index.html"
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
